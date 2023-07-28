c82092c147a5979bdda6edf91fcf9f3164aaa6d1
joseg <josegalviarez21.@gmail.com>
2023-07-28 15:53:38 -0400

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      blue:"#03a7d3",
+      red:"#ff4646",
+      graylight:"#999999"
+      },
+
+      fontFamily:{
+        roboto: ["Roboto, sans-serif "]
+      },
+
+     
+
+    },
+  },
+  plugins: [],
+};
+
+
+
